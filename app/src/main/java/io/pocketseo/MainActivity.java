e3a1b52628fdca2a2b52c01a8ccad7d32ff02cee
Pete Harris <peteh@thedistance.co.uk>
2016-02-17 11:42:34 +0000

    PSEO-13 implement test analytic event

--- app/src/main/java/io/pocketseo/MainActivity.java
+++ app/src/main/java/io/pocketseo/MainActivity.java
@@ -5,12 +5,16 @@
 package io.pocketseo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.crashlytics.android.Crashlytics;
+
+import java.util.Date;
+
+import uk.co.thedistance.thedistancekit.Analytics;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         if(BuildConfig.DEBUG){
             // show "force crash" option when in debug mode
             menu.findItem(R.id.action_crash).setVisible(true);
+            menu.findItem(R.id.action_test_event).setVisible(true);
         }
         return true;
     }
@@ -38,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.action_crash:
                 Crashlytics.getInstance().crash();
+                return true;
+            case R.id.action_test_event:
+                Analytics.send(this, new Analytics.AnalyticEvent("Test","Test",new Date().toString()));
                 return true;
             case R.id.action_about:
                 showAboutActivity();
--- app/src/main/java/io/pocketseo/PocketSeoApplication.java
+++ app/src/main/java/io/pocketseo/PocketSeoApplication.java
@@ -4,13 +4,13 @@
 
 package io.pocketseo;
 
-import android.app.Application;
-
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.analytics.GoogleAnalytics;
 
 import io.fabric.sdk.android.Fabric;
+import uk.co.thedistance.thedistancekit.TheDistanceApplication;
 
-public class PocketSeoApplication extends Application {
+public class PocketSeoApplication extends TheDistanceApplication {
 
 
     @Override
@@ -21,5 +21,14 @@ public class PocketSeoApplication extends Application {
         if (BuildConfig.USE_CRASHLYTICS /* && preferences.getBoolean(getString(R.string.pref_key_send_crashes), true) */) {
             Fabric.with(this, new Crashlytics());
         }
+    }
+
+    @Override
+    protected synchronized void initializeTracker() {
+//        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        if (mTracker == null && preferences.getBoolean("send_usage", true)) {
+            mTracker = GoogleAnalytics.getInstance(this)
+                    .newTracker(R.xml.global_tracker);
+//        }
     }
 }
